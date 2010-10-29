539f2a00f322e4c7887020eba23d455123cdcf43
Peter Robinson <peter.b.robinson@colorado.edu>
2010-10-28 20:22:50 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+
+# Bundle edge Rails instead:
+# gem 'rails', :git => 'git://github.com/rails/rails.git'
+
+gem 'mysql2'
+
+group :development do 
+	gem 'rspec-rails','2.0.1'
+end
+
+group :test do
+	gem 'rspec','2.0.1'
+	gem 'webrat', '0.7.1'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3611137010d00ed515f6d09e10b24cf73e6c62ea4b21671b59cb76950603845841ca2edf76503c828bbd681a11d8ebda42a6c747091aab18ea04014e3182608c'
